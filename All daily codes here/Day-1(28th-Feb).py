9add1d4ad76dd9d85edc9f4ef53f472ca95c8719
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-03-01 20:48:22 +0530

    Day 1 Updated

--- All daily codes here/Day-1(28th-Feb).py
+++ All daily codes here/Day-1(28th-Feb).py
@@ -1,5 +1,7 @@
 # 28th Feb notes
 
+# Below is a example of how to open a new tab in the browser using python, this is only for your reference, you can use this in your project if you want to open a new tab in the browser using python.
+
 import webbrowser
 
 def open_new_tab(url):
@@ -7,3 +9,5 @@ def open_new_tab(url):
 
 url = "https://www.google.com"
 open_new_tab(url)
+
+# This only to show what python can do if you really know how to use it.
