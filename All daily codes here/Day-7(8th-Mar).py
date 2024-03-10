96e4e04d4fb8b7eea152640efcfd4392522ef5db
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-03-10 15:10:41 +0530

    Day 7 uploaded

---
+++ All daily codes here/Day-7(8th-Mar).py
@@ -0,0 +1,35 @@
+# Day 7 notes
+
+'''''
+ # Bitwise operators
+      Bitwise and(&)
+      Bitwise or(|)
+      Bitwise xor(^)
+      
+      
+    i) Bitwise and(&) - If both bits are 1, then only the result is 1. Otherwise, it is 0.
+        ex - 79 & 83 = 1001111 & 1010011 = 1000011 = 67
+        truth table of and - 
+        0 & 0 = 0
+        0 & 1 = 0
+        1 & 0 = 0
+        1 & 1 = 1
+        
+    ii) Bitwise or(|) - If any of the bits is 1, then the result is 1. Otherwise, it is 0.
+        ex - 17 | 19 = 10001 | 10011 = 10011 = 19
+        truth table of or -
+        0 | 0 = 0
+        0 | 1 = 1
+        1 | 0 = 1
+        1 | 1 = 1
+        
+    iii) Bitwise xor(^) - If both bits are different, then the result is 1. Otherwise, it is 0.
+        ex - 12 ^ 13 = 1100 ^ 1101 = 0001 = 1
+             57 ^ 93 = 0111001 ^ 1011101 = 1100100 = 100
+        truth table of xor -
+        0 ^ 0 = 0
+        0 ^ 1 = 1
+        1 ^ 0 = 1
+        1 ^ 1 = 0  
+'''''
+
