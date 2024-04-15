7b0736cbf2d3c2ecf21b7760b9d9bccb52621169
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-04-15 18:45:40 +0530

    Day 17 updated

--- All daily codes here/Day-17(14th-Apr).py
+++ All daily codes here/Day-17(14th-Apr).py
@@ -1,4 +1,4 @@
-# Day 17
+# Day 17 notes
 
 # Functions in python 
 # Functions are a convenient way to divide your code into useful blocks, allowing us to order our code, make it more readable, reuse it and save some time. Also functions are a key way to define interfaces so programmers can share their code.
