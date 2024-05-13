64e3b4e115fa16a65348ba6ecfa114af4bcf0a4f
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-05-13 18:51:26 +0530

    Day 28 updated

--- All daily codes here/Day-28(6th-May).py
+++ All daily codes here/Day-28(6th-May).py
@@ -4,11 +4,12 @@
 
 # Map - map(function, iterable)
 # Map is a function that takes in two arguments: a function and a sequence iterable. In the form: map(function, sequence)
-# 
+#
 
 l = [21, 34, 56, 78, 90]
 lc = [l for i in l if i % 2 == 0]
-m = list(map(lambda i:i * 2 / 4, l))  # When we call map function, it is automatically calling lambda function for each element of the list.
+# When we call map function, it is automatically calling lambda function for each element of the list.
+m = list(map(lambda i: i * 2 / 4, l))
 
 # print(lc)
 # print(m)
@@ -17,7 +18,7 @@ m = list(map(lambda i:i * 2 / 4, l))  # When we call map function, it is automat
 # The filter() function in Python takes in a function and a list as arguments. This offers an elegant way to filter out all the elements of a sequence "sequence", for which the function returns True.
 #
 
-f = list(filter(lambda i:i % 2 == 0, l))
+f = list(filter(lambda i: i % 2 == 0, l))
 
 # print(f)
 
@@ -41,6 +42,7 @@ print(hof())
 # Zip, Enumerate
 
 # Zip - zip(iterable1, iterable2)
+# It brings corresponding elements of two iterables together in a tuple.
 
 l1 = [1, 2, 3, 4, 5]
 t = (1, 2, 3, 4, 5)
@@ -51,3 +53,11 @@ d = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five"}
 x = dict(zip(l1, t))
 
 # print(x)
+
+# Enumerate - enumerate(iterable, start=0)
+# It adds a counter to an iterable and returns it in a form of enumerate object.
+
+l = [12, 34, 56, 78, 90, 98, 76, 54, 32, 10]
+
+for index, value in enumerate(l):
+    print(index, value)
