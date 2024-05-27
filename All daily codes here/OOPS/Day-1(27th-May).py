f62d4f29f8c7f22f984e15111dbae6217734f451
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-05-27 19:19:25 +0530

    OOPS Day 1 updated

---
+++ All daily codes here/OOPS/Day-1(27th-May).py
@@ -0,0 +1,33 @@
+# OOPS Day 1 notes
+
+
+# PPP(Procedural Programming Paradigm)
+# In this, we write the code in a sequence of steps. It is a top-down approach. It increases the lines of code and decreases the reusability of the code.
+
+# FPP(Functional Programming Paradigm)
+# In this, we write the code in the form of functions. It is a bottom-up approach. It increases the reusability of the code and decreases the lines of code. It does not have security.
+
+# OOPS(Object-Oriented Programming Paradigm)
+# In this, we write the code in the form of classes and objects. It is a bottom-up approach. It increases the reusability of the code and decreases the lines of code. It encapsulates the data and functions. It has security.
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# OOPS Concepts
+
+# 1. Encapsulation
+# It is the process of binding the data and functions together in a class. It is also known as data hiding. It is used to restrict the access to the data and functions. It is used to protect the data from the outside world.
+# Ex - In this data leak is impossible, as it stops the extrernal access to the data and implements the security. User can only access the data through the functions. It uses class and objects.
+
+# 2. Abstraction or Data Abstraction/Hiding
+# It hides the complex implementation of the data and shows only the necessary details to the user. By only showing the necessary details, it increases the security of the data.
+
+# 3. Polymorphism
+# In this Poly means many and morphism means forms. It is the ability of a function to perform in different ways. 
+# Ex - "+" operator is used for addition and concatenation. It is used to perform the same function in different ways.
+
+# 4. Inheritance
+# It is the process of acquiring the properties of one class by another class. It is used to implement the parent-child relationship.
+
+# 5. Class & Object
+# Class is a blueprint of an object. It is a user-defined data type. It is a collection of objects. It is a template of an object. It is a logical entity. It is a reference data type.
+# Object is an instance of a class. It is a real entity. It is a physical entity. It is a memory entity. It is a value data type.
