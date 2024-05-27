a464349a9e438927face9ed26206ef80c9a1be3b
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-05-27 18:50:00 +0530

    File structured and OOPS programming added

---
+++ All daily codes here/Python/temp.py
@@ -0,0 +1,30 @@
+# import datetime
+# import time
+
+# print("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\nTwinkle, twinkle, little star,\n\t How I wonder what you are")
+
+# import sys
+# print(sys.version)
+
+# while True:
+#     now = datetime.datetime.now()
+#     print("Current date and time:", now)
+#     time.sleep(1)
+
+# r = float(input("Enter the radius of circle: "))
+# print("You have entered:", r)
+# carea = 22/7 * r * r
+# print("So, area of circle is:", carea)
+
+# first_name, last_name = input("Enter your full name: ").split()
+# reversed_name = last_name + " " + first_name
+# print("Reversed name:", reversed_name)
+
+# rnum = input("Enter the numbers: ").split()
+# r1 = list(rnum)
+# r2 = tuple(rnum)
+# print(r1,"It is a list\n",r2,"It is a tuple")
+
+# fn, fex = input("Enter a filename with its extension: ").split(".")
+# print("The extension name is","'", fex,"'")
+
