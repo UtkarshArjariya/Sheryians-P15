a603e6ae87bd2e31c1fbae7f765c9aa9040ad973
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-06-09 15:17:11 +0530

    Deleted soon

--- All daily codes here/Python/temp.py
+++ All daily codes here/Python/temp.py
@@ -1,30 +1,76 @@
-# import datetime
-# import time
+# Some day notes OOPS
 
-# print("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\nTwinkle, twinkle, little star,\n\t How I wonder what you are")
+import json
+import string
+import random
 
-# import sys
-# print(sys.version)
+class Bank:
+    database = "bank.json"
+    data = []
+    
+    
+    try:
+        with open(database) as fs:
+            data = json.loads(fs.read())
+    except Exception as err:
+        print(err)
+        
+                    
+    @classmethod
+    def updatedata(cls):
+        with open(cls.database,"w") as fs:
+            fs.write(json.dumps(cls.data))
+    
+        
+    @classmethod
+    def randomifsc(cls):
+        alpha = random.choices(string.ascii_letters,k = 3)
+        number = random.choices(string.digits,k = 3) 
+        spchar = random.choices("!@#$%^&*",k = 2)
+        id = alpha + number + spchar        
+        random.shuffle(id)
+        return "".join(id)
+    
+        
+    @classmethod
+    def createaccount(cls):
+        info = {
+            "name" : input("Enter your Name : "),
+        "email" : input("Enter your email ID : "),
+        "number" : int(input("Enter your mobile Number : ")),
+        "Account Number" : cls.randomifsc(),
+        "pin" : int(input("please tell your 4-digit pin : ")),
+        "balance" : 0,
+        }
+        cls.data.append(info)
+        cls.updatedata()
 
-# while True:
-#     now = datetime.datetime.now()
-#     print("Current date and time:", now)
-#     time.sleep(1)
 
-# r = float(input("Enter the radius of circle: "))
-# print("You have entered:", r)
-# carea = 22/7 * r * r
-# print("So, area of circle is:", carea)
+    @classmethod
+    def depositemoney(cls):
+        an = input("Enter your account number : ")
+        pin = int(input("please tell your pin : "))
+        user = [i for i in cls.data if i["Account Number"] == an and i["pin"] == pin]
+        
+        if not user:
+            print("no user found")
+        else:
+            print(f"Your Current balance : {user[0]["balance"]}")
+            amt = int(input("How much amount you want to deposit : "))
+            if amt > 9999:
+                print("Sorry you cannot deposit this much.")
+            else:
+                user[0]["balance"] += amt 
+                print("Amount added successfully.")
+                Bank.updatedata()
+            
+            
+print("press 1 for creating an account : ")
+print("press 2 to Deposit money : ")
 
-# first_name, last_name = input("Enter your full name: ").split()
-# reversed_name = last_name + " " + first_name
-# print("Reversed name:", reversed_name)
-
-# rnum = input("Enter the numbers: ").split()
-# r1 = list(rnum)
-# r2 = tuple(rnum)
-# print(r1,"It is a list\n",r2,"It is a tuple")
-
-# fn, fex = input("Enter a filename with its extension: ").split(".")
-# print("The extension name is","'", fex,"'")
-
+check = input("Enter your choice : ")
+    
+if check == "1" :
+    Bank.createaccount()    
+elif check == "2":
+    Bank.depositemoney()
