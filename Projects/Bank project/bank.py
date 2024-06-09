fae262ccee3058acdc2c26f876c8768f24305e2d
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2024-06-09 15:49:18 +0530

    Bank project updated as add update function when your press 4

--- Projects/Bank project/bank.py
+++ Projects/Bank project/bank.py
@@ -1,12 +1,21 @@
-# Some day notes OOPS
-
 import json
 import string
 import random
+import os
+
+# Some day notes OOPS
+
 
 class Bank:
-    database = "bank.json"
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    database = os.path.join(current_dir, "bank.json")
     data = []
+
+    try:
+        with open(database) as fs:
+            data = json.loads(fs.read())
+    except Exception as err:
+        print(err)
     
     try:
         with open(database) as fs:
@@ -73,10 +82,31 @@ class Bank:
                 user[0]["balance"] -= amt 
                 print("Amount withdrawn successfully.")
                 Bank.updatedata()
+    
+    @classmethod
+    def updatebankdetails(cls):
+        an = input("Enter your account number : ")
+        pin = int(input("please tell your pin : "))
+        user = [i for i in cls.data if i["Account Number"] == an and i["pin"] == pin]
+        
+        if not user:
+            print("no user found")
+        else:
+            print(f"Your Current details : {user[0]}")
+            info = {
+                "name" : input("Enter your updated Name : "),
+                "email" : input("Enter your updated email ID : "),
+                "number" : int(input("Enter your updated mobile Number : ")),
+                "pin" : int(input("please tell your updated 4-digit pin : ")),
+            }
+            user[0].update(info)
+            print("Bank details updated successfully.")
+            Bank.updatedata()
 
 print("press 1 for creating an account : ")
 print("press 2 to Deposit money : ")
 print("press 3 to Withdraw money : ")
+print("press 4 to Update bank details : ")
 
 check = input("Enter your choice : ")
     
@@ -86,5 +116,7 @@ elif check == "2":
     Bank.depositemoney()
 elif check == "3":
     Bank.withdrawmoney()
+elif check == "4":
+    Bank.updatebankdetails()
 else:
     print("Invalid choice.")
